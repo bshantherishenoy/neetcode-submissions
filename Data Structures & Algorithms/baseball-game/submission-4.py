547bfae3d95442652a9650958052a0c1c76b2606
Shantheri shenoy <63908336+bshantherishenoy@users.noreply.github.com>
2026-05-20 16:59:19 +0530

    Add: baseball-game - submission-4

---
+++ Data Structures & Algorithms/baseball-game/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        special_chars = ['+' , 'D' , 'C']
+        for i in operations:
+            if i.isnumeric():
+                stack.append(int(i))
+                print(stack)
+            elif i.startswith("-"):
+                stack.append(int(i))
+                print(stack)
+            elif i in special_chars:
+                if i == "+" and len(stack) >= 2:
+                    num1 = stack.pop()
+                    num2 = stack.pop()
+                    stack.extend([num2,num1,num1+num2])
+                    print(stack)
+                elif i == "D" and len(stack)>=1:
+                    num1 = stack[-1]
+                    stack.append(num1*2)
+                    print(stack)
+                elif i == "C" and len(stack):
+                    stack.pop()
+                    print(stack)
+                else:
+                    print(f"Invalid operation:{i}\n Length of stack:{len(stack)}")
+        return sum(stack)
