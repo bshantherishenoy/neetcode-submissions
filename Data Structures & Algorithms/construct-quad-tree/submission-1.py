685f2ceb9540289340035f41fe4a94e6436eeaa6
Shantheri shenoy <63908336+bshantherishenoy@users.noreply.github.com>
2026-08-02 12:16:36 +0530

    Add: construct-quad-tree - submission-1

---
+++ Data Structures & Algorithms/construct-quad-tree/submission-1.py
@@ -0,0 +1,47 @@
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+        # use a grid divide to half and then check the subsequent nodes are equal to first if not create children for the same else keep going ahead.
+        # revisit this problem when you have some time 
+        def builder(r,c,size):
+            first = grid[r][c]
+            for i in range(r,r+size):
+                for j in range(c,c+size):
+                    if grid[i][j] != first:
+                        half = size//2
+                        tl = builder(r,c,half)
+                        tr = builder(r,c+half,half)
+                        bl = builder(r+half,c,half)
+                        br = builder(r+half,c+half,half)
+                        return Node(
+                                val=True,
+                                isLeaf=False,
+                                topLeft=tl,
+                                topRight=tr,
+                                bottomLeft=bl,
+                                bottomRight=br
+                                )
+                    
+            return Node(
+                    val=(first == 1),
+                    isLeaf=True,
+                    topLeft=None,
+                    topRight=None,
+                    bottomLeft=None,
+                    bottomRight=None
+                    )
+        n = len(grid)
+        return builder(0,0,n)
+
+        
